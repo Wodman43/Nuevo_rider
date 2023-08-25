711adc728c8075ad2b52965ed2c74c2cf068c9a4
wodman43 <lopezquira10gmail.com>
2023-08-25 18:22:17 -0500

    a

--- js/main_notas2.js
+++ js/main_notas2.js
@@ -1,8 +1,32 @@
 const  midiv = document.getElementById('midiv');
 // const mibtn = document.getElementById('mibtn');
+const tabla = document.getElementById('tabla');
+
+let array = [];
+
 
 const settext = data=>{
-midiv.textContent = data;
+array.forEach(e=>{
+    console.log(e);
+    let tablita = document.createElement('tr');
+    let ident = document.createElement('td');
+    ident.innerHTML = `<td>${e.id}</td>`;
+    let nombre = document.createElement('td');
+    nombre.innerHTML =`<td>${e.nom}</td>`;
+    let nota1 = document.createElement('td');
+    nota1.innerHTML =`<td>${e.nota[0]}</td>`;
+    let nota2 = document.createElement('td');
+    nota2.innerHTML =`<td>${e.nota[1]}</td>`;
+    let nota3 = document.createElement('td');
+    nota3.innerHTML =`<td>${e.nota[2]}</td>`;
+    tablita.appendChild(ident);
+    tablita.appendChild(nombre);
+    tablita.appendChild(nota1);
+    tablita.appendChild(nota2);
+    tablita.appendChild(nota3);
+    tabla.appendChild(tablita);
+    // midiv.innerText = data;
+})
 }
 const getdata = () =>{
     return new Promise((resuleve,rechaza)=>{
@@ -12,14 +36,21 @@ const getdata = () =>{
     })
 }
 
-        const showdata = (notas) =>{
-            let objeto = {};
-            notas.forEach(e =>{
-                let nose = 'nota'+e;
-                objeto[nose] = e;
-            });
-          
-            console.log(objeto);
+        const showdata = (notas,id,nom) =>{
+            let objeto = {
+                id:id,
+                nom:nom,
+                nota:notas,
+            };
+            // let a = 0;
+            // notas.forEach(e =>{
+           
+            //     let nose = a
+            //     a++;
+            //     objeto[nose] = e;
+            // })
+
+            array = [...array,objeto]
 
             return new Promise((resuleve1,rechaza1)=>{
             settext('Esperando Autorizacion');
@@ -45,10 +76,10 @@ const getdata = () =>{
         })
         }
 
-        const boton =(nota1,nota2,nota3)=>{
+        const boton =(nota1,nota2,nota3,id,nom)=>{
             getdata().then(permitido=>{
                 if(permitido){
-                   return showdata([nota1,nota2,nota3]);
+                   return showdata([nota1,nota2,nota3],id,nom);
                     }
                 
         }).then(user=>{
